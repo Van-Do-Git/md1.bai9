b22cf5aa3abf3ae992a05a2b18e94a15c997ff6f
unknown <mittervan@gmail.com>
2021-06-11 10:26:16 +0700

    may tinh co ban

---
+++ maytinh.js
@@ -0,0 +1,65 @@
+function o(){
+    let sonhapvao=document.getElementById("manhinh").innerText;
+    document.getElementById("manhinh").innerText=sonhapvao+"0";
+}
+function mot(){
+    let sonhapvao=document.getElementById("manhinh").innerText;
+    document.getElementById("manhinh").innerText=sonhapvao+"1";
+}
+function hai(){
+    let sonhapvao=document.getElementById("manhinh").innerText;
+    document.getElementById("manhinh").innerText=sonhapvao+"2";
+}
+function ba(){
+    let sonhapvao=document.getElementById("manhinh").innerText;
+    document.getElementById("manhinh").innerText=sonhapvao+"3";
+}
+function bon(){
+    let sonhapvao=document.getElementById("manhinh").innerText;
+    document.getElementById("manhinh").innerText=sonhapvao+"4";
+}
+function nam(){
+    let sonhapvao=document.getElementById("manhinh").innerText;
+    document.getElementById("manhinh").innerText=sonhapvao+"5";
+}
+function sau(){
+    let sonhapvao=document.getElementById("manhinh").innerText;
+    document.getElementById("manhinh").innerText=sonhapvao+"6";
+}
+function bay(){
+    let sonhapvao=document.getElementById("manhinh").innerText;
+    document.getElementById("manhinh").innerText=sonhapvao+"7";
+}
+function tam(){
+    let sonhapvao=document.getElementById("manhinh").innerText;
+    document.getElementById("manhinh").innerText=sonhapvao+"8";
+}
+function chin(){
+    let sonhapvao=document.getElementById("manhinh").innerText;
+    document.getElementById("manhinh").innerText=sonhapvao+"9";
+}
+function c(){
+    document.getElementById("manhinh").innerText="";
+}
+function cong(){
+    let sonhapvao=document.getElementById("manhinh").innerText;
+    document.getElementById("manhinh").innerText=sonhapvao+"+";
+}
+function tru(){
+    let sonhapvao=document.getElementById("manhinh").innerText;
+    document.getElementById("manhinh").innerText=sonhapvao+"-";
+}
+function nhan() {
+    let sonhapvao=document.getElementById("manhinh").innerText;
+    document.getElementById("manhinh").innerText=sonhapvao+"*";
+
+}
+function chia() {
+    let sonhapvao=document.getElementById("manhinh").innerText;
+    document.getElementById("manhinh").innerText=sonhapvao+"/";
+}
+function bang() {
+    let sonhapvao=document.getElementById("manhinh").innerText;
+    let ketqua=eval(sonhapvao)
+    document.getElementById("manhinh").innerText= ketqua;
+}
